ef5f498bb8a01d2b6ec0b44d774df97b33be3d78
Cranio290 <143084676+Cranio290@users.noreply.github.com>
2024-09-30 22:03:31 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "build",
-    emptyOutDir: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.KEY': JSON.stringify(env.KEY),
+      'process.env.IV': JSON.stringify(env.IV),
+    },
+    build: {
+      outDir: "build",
+      emptyOutDir: true,
+    },
 
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
+      strictPort: true,
+    }
   }
 });
