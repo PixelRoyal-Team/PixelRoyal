1a584c71bccfb69c8f9ee43124b628496430dc59
Cranio290 <143084676+Cranio290@users.noreply.github.com>
2024-10-05 22:32:47 +0200

    Add files via upload

---
+++ pixelroyal/src/ui/Menu/Support.jsx
@@ -0,0 +1,51 @@
+export default function Support () {
+  return (
+    <>
+      <p>You can contact us here at our <b><a style={{color: "#7289DA"}} href="https://discord.gg/ThrY58x6Qc" target="__blank">Discord</a></b>.
+
+        <h3>PixelRoyal Owners:</h3>
+        <ul>
+          <li>Cranio</li>
+          <li>Mike</li>
+          <li>Patriotis</li>
+        </ul>
+
+        <h3>PixelRoyal Developers:</h3>
+        <ul>
+          <li>Cancel</li>
+          <li>Cranio</li>
+          <li>Gigio</li>
+          <li>Marcouz</li>
+        </ul>
+        
+        <h3>PixelRoyal Admins:</h3>
+        <ul>
+          <li>Ahmetaydal</li>
+          <li>Kostas</li>
+          <li>LukaGeo</li>
+          <li>Nickoetsi</li>
+          <li>Saviox</li>
+          <li>solak1s</li>
+          <li>st0_0ts</li>
+          <li>ThisLuka</li>
+          <li>g2</li>
+          <li>ⲉⲗⲗⲏⲛ ⲟⲧⲧⲟ</li>
+        </ul>
+
+        <h3>PixelRoyal Moderators:</h3>
+        <ul>
+          <li>Ali</li>
+          <li>Anesta</li>
+          <li>Gigio</li>
+          <li>id4l</li>
+          <li>misho</li>
+          <li>nyxtgy</li>
+          <li>soca</li>
+          <li>sugawara</li>
+          <li>viktoras</li>
+          <li>zubaiyan</li>
+        </ul>
+      </p>
+    </>
+  )
+}
