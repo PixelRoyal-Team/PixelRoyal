d6d88460735299302adbc044735f91044ff2bc94
Cranio290 <143084676+Cranio290@users.noreply.github.com>
2024-10-05 19:36:01 +0200

    Delete pixelroyal directory

--- pixelroyal/src/index.js
+++
@@ -1,6 +0,0 @@
-// src/index.js
-import App from './App';
-
-const container = document.getElementById('root');
-const appElement = App(); // Call the App function to get the element
-container.appendChild(appElement); // Append the element to the container
